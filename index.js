1687221cc8d97896719d0a665f1c791c0b068de6
samuelconlon <conlonsp@yahoo.com>
2025-01-07 15:47:29 -1000

    commit tree c422724e883523e41319111b25be50317dcc9121

--- index.js
+++ index.js
@@ -1,2 +1,49 @@
 function addingEventListener() {
+  const input = document.getElementById('button');
+  return input.addEventListener('click', function() {
+    alert('I was clicked!')
+  })
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function addingEventListener() {
+//   const input = document.getElementById('button')
+//   return input.addEventListener('click', () => alert("I've been clicked!"))
+// }
+
+// addingEventListener()
